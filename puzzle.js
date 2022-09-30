8d34c09832f2fff0068c665dab35be73d829e507
becca-bear <you@example.com>
2022-09-30 15:24:52 -0400

    main

--- puzzle.js
+++ puzzle.js
@@ -14,7 +14,15 @@ window.onload = function() {
             let tile = document.createElement("img");
             tile.src = "./images/blank.jpg";
 
-            document.getElementById("board").append(tile);
+          //DRAG FUNCTIONALITY
+          tile.addEventListener("dragstart", dragStart); //click image to drag
+          tile.addEventListener("dragover", dragOver); //drag an image
+          tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+          tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+          tile.addEventListener("drop", dragDrop); //drop an image onto another one
+          tile.addEventListener("dragend", dragEnd); //after you completed dragDrop
+
+          document.getElementById("board").append(tile);
         }
     }
 
@@ -25,7 +33,7 @@ window.onload = function() {
     }
     pieces.reverse();
     for (let i=0; i < pieces.length; i++) {
-        let j = Math.floor(Math.random() * pieces.length )
+        let j = Math.floor(Math.random() * pieces.length );
     //swap
         let tmp = pieces[i]
         pieces[i] = pieces[j]
@@ -36,8 +44,53 @@ window.onload = function() {
     for (let i = 0; i < pieces.length; i++) {
         let tile = document.createElement("img");
         tile.src = `./images/image_part_${pieces[i].padStart(3,0)}.jpg`
+
+          //DRAG FUNCTIONALITY
+          tile.addEventListener("dragstart", dragStart); //click image to drag
+          tile.addEventListener("dragover", dragOver); //drag an image
+          tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+          tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+          tile.addEventListener("drop", dragDrop); //drop an image onto another one
+          tile.addEventListener("dragend", dragEnd); //after you completed dragDrop
+          document.getElementById("board").append(tile);
+
+      
         document.getElementById("pieces").append(tile);
 
         
+
+
     }
 }
+
+
+//DRAG TILES
+function dragStart() {
+    currTile = this; //this refers to image that was clicked on for dragging
+}
+
+    function dragOver(e) {
+        e.preventDefault();
+
+    }
+    
+    function dragEnter(e) {
+        e.preventDefault();
+    
+    }
+    function dragLeave() {
+
+    }
+
+    function dragDrop() {
+        otherTile = this; //this refers to image that is being dropped on
+    }
+    function dragEnd() {
+        let currImg = currTile.src;
+        let otherImg= otherTile.src;
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+
+        turns += 1;
+        document.getElementById("turns").innerText = turns;
+    }
